bba0901c7edaba36ef8ed995ec3cdab0afb8bad9
MatrixVVhite <yotamoscohen@gmail.com>
2024-07-05 00:24:44 +0300

    Copied Project

---
+++ Demo/Demo.cs
@@ -0,0 +1,181 @@
+﻿using Positioning;
+using NewTilemapEngine;
+using System.Net.Http.Headers;
+using CoreEngineHierarchy;
+using Commands;
+using System.Numerics;
+
+namespace Demo
+{
+    class Demo
+    {
+        private static Demo _instance;
+
+        /// <summary>
+        /// singletone
+        /// </summary>
+        /// <returns></returns>
+        public static Demo GetInstance()
+        {
+            return _instance ?? (_instance = new Demo());
+        }
+        public TileObject MakeQueen(TileObject toPromote)
+        {
+            TileObject queen = new TileObject("Q", new List<Position>(), toPromote.CurrentPos, toPromote.Owner);
+            queen.Positions.Clear();
+            var mapBounds = TheEngine.GetInstance()._addTiles.TileMapMatrix.GetLength(1) - 1;
+            int counter = 1;
+            for (int i = 0; i < TheEngine.GetInstance()._addTiles.TileMapMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < TheEngine.GetInstance()._addTiles.TileMapMatrix.GetLength(1); j++)
+                {
+                    if (TheEngine.GetInstance()._addTiles.TileMapMatrix[i, j].TilePos.X < TheEngine.GetInstance()._addTiles.TileMapMatrix.GetLength(0) - 1 &&
+                    TheEngine.GetInstance()._addTiles.TileMapMatrix[i, j].TilePos.Y < TheEngine.GetInstance()._addTiles.TileMapMatrix.GetLength(1) - 1 &&
+                    TheEngine.GetInstance()._addTiles.TileMapMatrix[i, j].TilePos.X > 0 && TheEngine.GetInstance()._addTiles.TileMapMatrix[i, j].TilePos.Y > 0)
+                    {
+                        if (toPromote.CurrentPos.X - toPromote.CurrentPos.Y == i - j || toPromote.CurrentPos.X + toPromote.CurrentPos.Y == i + j)
+                        {
+                            queen.Positions.Add(new Position(i - toPromote.CurrentPos.X, j - toPromote.CurrentPos.Y));
+                        }
+                        //queen.Positions.Add(new Position((-i) * toPromote.Owner.MovesToX, (+j) * toPromote.Owner.MovesToY)); //upper left
+                        //queen.Positions.Add(new Position((+i) * toPromote.Owner.MovesToX, (+j) * toPromote.Owner.MovesToY));//upper right
+                        //queen.Positions.Add(new Position((+i) * toPromote.Owner.MovesToX, (-j) * toPromote.Owner.MovesToY));//down right
+                        //queen.Positions.Add(new Position((-i) * toPromote.Owner.MovesToX, (-j) * toPromote.Owner.MovesToY));// down left
+                    }
+                }
+            }
+            //for (int j = 1; j < mapBounds; j++)
+            //{
+
+            //    queen.Positions.Add( new Position(( -counter) * toPromote.Owner.MovesToX,   (+ j) * toPromote.Owner.MovesToY)); //upper left
+            //    queen.Positions.Add(new Position(( + counter) * toPromote.Owner.MovesToX,  (+ j) * toPromote.Owner.MovesToY));//upper right
+            //    queen.Positions.Add(new Position(( +  counter) * toPromote.Owner.MovesToX, (- j) * toPromote.Owner.MovesToY));//down right
+            //    queen.Positions.Add(new Position(( -  counter) * toPromote.Owner.MovesToX, (- j) * toPromote.Owner.MovesToY));// down left
+            //    counter++;
+            //}
+            queen.IsQueen = true;
+            return queen;
+        }
+
+
+        string _choice_player;
+
+        //Make the first scene
+        public void OpenScene()
+        {
+            Console.WriteLine();
+            TheEngine.GetInstance().CreateHeadLine("Checkers");
+            Console.WriteLine();
+
+            string _new_game = "------ To start a game enter 1 ------";
+
+            Console.Write(new string(' ', (Console.WindowWidth - _new_game.Length) / 2));
+            Console.WriteLine(_new_game);
+
+            Console.WriteLine();
+
+            string _exit_game = "------ To exit game enter 2 ------";
+
+            Console.Write(new string(' ', (Console.WindowWidth - _exit_game.Length) / 2));
+            Console.WriteLine(_exit_game);
+
+            _choice_player = Console.ReadLine();
+        }
+
+        public void RunGame()
+        {
+            OpenScene();
+
+            if (_choice_player == "1")
+            {
+                Start_Game();
+            }
+
+            else if (_choice_player == "2")
+            {
+                Environment.Exit(0);
+            }
+
+            else
+            {
+                Console.WriteLine("There is no optation like this, try again.");
+                _choice_player = Console.ReadLine();
+            }
+
+        }
+
+
+        public void Start_Game()
+        {
+            //HundleTurns.setNumberOfPlayers(2);
+            HundleTurns.CurrentPlayer = 1;
+            TheEngine.GetInstance()._players = new List<Player>() { new Player(1, ConsoleColor.Red, 1, 1), new Player(2, ConsoleColor.Blue, 1, -1) };
+            CommandHandler.MyMovemetHandler.CanEat = CheckersEatingCheck;
+            HundleTurns.AddTurns(1, 2);
+            List<TileObject> list1 = new List<TileObject>();
+            List<TileObject> list2 = new List<TileObject>();
+
+            for (int i = 1; i < TheEngine.GetInstance()._addTiles.TileMapMatrix.GetLength(0) - 1; i++)
+            {
+                for (int j = 1; j < 4; j++)
+                {
+                    if ((i % 2 == 0 && j % 2 == 1) || (i % 2 == 1 && j % 2 == 0))
+                    {
+                        Position pos = new Position(i, j);
+
+                        TheEngine.GetInstance().Template_Checkers("checkers", "black", pos);
+                        list1.Add(TheEngine.GetInstance().CreateObjectForFirstPlayer(TheEngine.GetInstance().GetPieces("Checkers"), pos));
+                    }
+                }
+
+                for (int j = TheEngine.GetInstance()._addTiles.TileMapMatrix.GetLength(1) - 4; j < TheEngine.GetInstance()._addTiles.TileMapMatrix.GetLength(1) - 1; j++)
+                {
+                    if ((i % 2 == 0 && j % 2 == 1) || (i % 2 == 1 && j % 2 == 0))
+                    {
+                        Position pos = new Position(i, j);
+
+                        TheEngine.GetInstance().Template_Checkers("checkers", "white", pos);
+                        list2.Add(TheEngine.GetInstance().CreateObjectForSecondPlayer(TheEngine.GetInstance().GetPieces("Checkers"), pos));
+                    }
+                }
+            }
+
+            TheEngine.GetInstance()._players[0].AddPieces(list1);
+            TheEngine.GetInstance()._players[1].AddPieces(list2);
+
+            //var myQueen =    MakeQueen(TheEngine.GetInstance()._addTiles.TileMapMatrix[2, 3].CurrentTileObject);
+            //TheEngine.GetInstance()._addTiles.DeleteTileObjectAtPos(new Position(2,3));
+            //TheEngine.GetInstance()._addTiles.InsertObjectToMap(myQueen, TheEngine.GetInstance()._addTiles.TileMapMatrix[2, 3].CurrentTileObject.CurrentPos);
+
+            TheEngine.GetInstance().UpdateBoard();
+            Commands.CommandHandler.HandleCommands();
+
+            while (true)
+            {
+
+                Console.SetCursorPosition(TheEngine.GetInstance()._addTiles.TileMapMatrix.GetLength(0) + 25, 1);
+                Console.WriteLine($"Player {HundleTurns.CurrentPlayer}'s Turn");
+                Console.SetCursorPosition(0, TheEngine.GetInstance()._addTiles.TileMapMatrix.GetLength(1) + 2);
+
+
+                _choice_player = Console.ReadLine();
+                Commands.CommandHandler.DiagnoseCommand(_choice_player, TheEngine.GetInstance()._addTiles, TheEngine.GetInstance()._renderingEngine);
+            }
+        }
+
+        public bool CheckersEatingCheck(TileObject hungryTile, TileObject threatenedTile)
+        {
+            if (threatenedTile.CurrentPos.X < hungryTile.CurrentPos.X)
+            {
+                if (TheEngine.GetInstance()._addTiles.TileMapMatrix[threatenedTile.CurrentPos.X - 1, threatenedTile.CurrentPos.Y + hungryTile.Owner.MovesToY].CurrentTileObject.TileObjectChar == " ")
+                    return true;
+            }
+            else if (threatenedTile.CurrentPos.X > hungryTile.CurrentPos.X)
+            {
+                if (TheEngine.GetInstance()._addTiles.TileMapMatrix[threatenedTile.CurrentPos.X + 1, threatenedTile.CurrentPos.Y + hungryTile.Owner.MovesToY].CurrentTileObject.TileObjectChar == " ")
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ NewTilemapEngine/TheEngine.cs
@@ -0,0 +1,237 @@
+﻿using Rendering;
+using CoreEngineHierarchy;
+using Positioning;
+using MovementAndInteraction;
+using Commands;
+using System.Reflection.Metadata.Ecma335;
+
+
+namespace NewTilemapEngine
+{
+    public class TheEngine
+    {
+        public int InitialSizeX { get; set; } = 10; // default value
+        public int InitialSizeY { get; set; } = 10; // default value
+
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+        private static TheEngine _instance;
+        public RenderingEngine _renderingEngine { get; private set; }
+        public TileMap _addTiles { get; private set; }
+
+        public List<Player> _players = new List<Player>();
+
+        /// <summary>
+        /// Template of an headline
+        /// </summary>
+        /// <param name="Title"></param>
+        public void CreateHeadLine(string Title)
+        {
+            Console.WriteLine("   _   _   _   _   _    ");
+            Console.WriteLine("  / \\ / \\ / \\ / \\ / \\   ");
+            Console.WriteLine("     " + Title + "   ");
+            Console.WriteLine("  \\_/ \\_/ \\_/ \\_/ \\_/   ");
+        }
+
+
+        /// <summary>
+        /// singletone
+        /// </summary>
+        /// <returns></returns>
+        private TheEngine()
+        {
+            _renderingEngine = new RenderingEngine(InitialSizeX, InitialSizeY);
+            _addTiles = new TileMap(InitialSizeX, InitialSizeY);
+        }
+
+        public static TheEngine GetInstance()
+        {
+            return _instance ?? (_instance = new TheEngine());
+        }
+
+        /// <summary>
+        /// Initialize of necessery parameters
+        /// </summary>
+        /// <returns></returns>
+        public void Initialize(int sizeX, int sizeY)
+        {
+            HundleTurns.CurrentPlayer = 1;
+            InitialSizeX = sizeX + 2;
+            InitialSizeY = sizeY + 2;
+            _renderingEngine = new RenderingEngine(InitialSizeX, InitialSizeY);
+            _addTiles = new TileMap(InitialSizeX, InitialSizeY);
+            for (int i = 0; i < HundleTurns.NumberOfPlayers; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    _players.Add(new Player(i + 1, ConsoleColor.Cyan, 1, 1));
+                }
+                else
+                {
+                    _players.Add(new Player(i + 1, ConsoleColor.Red, 1, -1));
+                }
+            }
+            SetCharList();
+        }
+        public void SetCharList()
+        {
+            CommandHandler.MyMovemetHandler.chars = new List<char>();
+            for (int i = 0; i < InitialSizeX; i++)
+            {
+                CommandHandler.MyMovemetHandler.chars.Add(alphabet[i]);
+            }
+        }
+
+        /// <summary>
+        /// Set the chess size (max size is 8 by 8)
+        /// </summary>
+        /// <returns></returns>
+        public RenderingEngine InitializeChessBoard(int sizeX, int sizeY)
+        {
+            if (sizeX >= 8) { sizeX = 8; }
+            if (sizeY >= 8) { sizeY = 8; }
+            Initialize(sizeX, sizeY);
+
+            return _renderingEngine;
+        }
+
+
+        /// <summary>
+        /// Update board
+        /// </summary>
+        /// <returns></returns>
+        public void UpdateBoard()
+        {
+            _renderingEngine.UpdateAndRender(_addTiles);
+        }
+
+        /// <summary>
+        /// Create commands
+        /// </summary>
+        /// <returns></returns>
+        public void CreateCommand()
+        {
+            //CommandHandler.HandleCommands()
+        }
+
+
+        /// <summary>
+        /// Allow the consumer an order to move the player
+        /// </summary>
+        /// <returns></returns>
+        public void MoveTo(Position position)
+        {
+        }
+
+        /// <summary>
+        /// Template of checkers that the player could use if want to or put any other string
+        /// </summary>
+        /// <returns></returns>
+        public void Template_Checkers(string gameType, string pieceColor, Position position)
+        {
+            char pieces = GetPieces(gameType);
+            ConsoleColor pieceConsoleColor = GetPieceColor(pieceColor);
+
+
+
+        }
+        public TileObject CreateObjectForFirstPlayer(char pieces, Position pos)
+        {
+            TileObject to1 = new TileObject(pieces.ToString(), new List<Position> { new Position(1 * _players[0].MovesToX, 1 * _players[0].MovesToY), new Position(-1 * _players[0].MovesToX, 1 * _players[0].MovesToY) }, pos, _players[0]);
+            _addTiles.InsertObjectToMap(to1, pos);
+            return to1;
+        }
+        public TileObject CreateObjectForSecondPlayer(char pieces, Position pos)
+        {
+            TileObject to1 = new TileObject(pieces.ToString(), new List<Position> { new Position(1 * _players[1].MovesToX, 1 * _players[1].MovesToY), new Position(-1 * _players[1].MovesToX, 1 * _players[1].MovesToY) }, pos, _players[1]);
+            _addTiles.InsertObjectToMap(to1, pos);
+            return to1;
+        }
+
+        /// <summary>
+        /// Piece template
+        /// </summary>
+        /// <returns></returns>
+        public char GetPieces(string gameType)
+        {
+            switch (gameType.ToLower())
+            {
+                case "checkers":
+                    return 'o';
+
+                default:
+                    throw new ArgumentException("Invalid game type", nameof(gameType));
+            }
+        }
+
+        /// <summary>
+        /// Color template
+        /// </summary>
+        /// <returns></returns>
+        private ConsoleColor GetPieceColor(string pieceColor)
+        {
+            switch (pieceColor.ToLower())
+            {
+                case "black":
+                    return ConsoleColor.Black;
+                case "white":
+                    return ConsoleColor.DarkYellow;
+                default:
+                    throw new ArgumentException("Invalid piece color", nameof(pieceColor));
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// All the methods that connected to the player
+    /// </summary>
+    public static class HundleTurns
+    {
+        public static int NumberOfmovesEachTurn;
+        public static int NumberOfPlayers;
+        public static int CurrentPlayer;
+
+        static event Action PlayerActions = () => { };
+
+        public static void AddTurns(int numberOfmovesEachTurn, int numberOfPlayers_AI)
+        {
+            NumberOfmovesEachTurn = numberOfmovesEachTurn;
+            NumberOfPlayers = numberOfPlayers_AI;
+            SubscribeEventOfTheClass();
+            PlayerActions?.Invoke();
+        }
+
+        internal static void SubscribeEventOfTheClass()
+        {
+            PlayerActions += () => HandleTurns();
+        }
+
+        internal static void HandleTurns()
+        {
+            for (int i = 0; i < NumberOfPlayers; i++)
+            {
+                if (i > 0)
+                {
+                    Console.WriteLine("Player Turn over");
+                }
+                for (int j = 0; j < NumberOfmovesEachTurn; j++)
+                {
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method to set the number of actors in the game
+        /// </summary>
+        /// <param name="numberOfActors"></param>
+        /// <returns></returns>
+        public static int setNumberOfPlayers(int numberOfActors)
+        {
+            int NumberOfActors = numberOfActors;
+
+            return NumberOfActors;
+        }
+    }
+}
